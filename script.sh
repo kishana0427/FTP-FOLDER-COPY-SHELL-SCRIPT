10bd6c7595dd07ea1f5e4b7031413d87acf8b0b4
kishana0427 <95524882+kishana0427@users.noreply.github.com>
2024-12-12 11:42:06 +0530

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,13 +1,84 @@
 #!/bin/bash
+HOST=mentorserver
+UNAME=mrcetece
+PSWD="ece@mrcet"
+mkdir -p /home/MentorGraphics/{ams,calibre,nitro,cshrc,Pictures,tanner}/
+chmod -R 777 /home/MentorGraphics/{ams,calibre,nitro,cshrc,Pictures,tanner}/
+mkdir -p /home/mrcetece/Tanner_EDA/
+chmod -R 777 /home/mrcetece/Tanner_EDA/
 
-# FTP server details
-FTP_SERVER="ftp.example.com"
-FTP_USER="your_username"
-FTP_PASS="your_password"
-REMOTE_DIR="/path/to/remote/folder"
-LOCAL_DIR="/path/to/local/destination"
+cd /home/MentorGraphics/cshrc/
 
-# Use wget to download the folder recursively
-wget --ftp-user="$FTP_USER" --ftp-password="$FTP_PASS" \
-     -r -nH --cut-dirs=1 --no-parent \
-     -P "$LOCAL_DIR" "ftp://$FTP_SERVER$REMOTE_DIR"
+ftp -niv $HOST << EOF
+user $UNAME $PSWD
+cd /home/MentorGraphics/mentorsoftware/cshrc/
+bin
+get hep.cshrc
+bye
+EOF
+
+cd /home/MentorGraphics/ams/
+
+ftp -niv $HOST << EOF
+user $UNAME $PSWD
+cd /home/MentorGraphics/mentorsoftware/ams/
+bin
+mget *
+bye
+EOF
+
+cd /home/MentorGraphics/nitro/
+
+ftp -niv $HOST << EOF
+user $UNAME $PSWD
+cd /home/MentorGraphics/mentorsoftware/nitro/
+bin
+mget *
+bye
+EOF
+
+cd /home/MentorGraphics/tanner/
+
+ftp -niv $HOST << EOF
+user $UNAME $PSWD
+cd /home/MentorGraphics/mentorsoftware/tanner/
+bin
+mget *
+bye
+EOF
+
+cd /home/MentorGraphics/calibre/
+
+ftp -niv $HOST << EOF
+user $UNAME $PSWD
+cd /home/MentorGraphics/mentorsoftware/calibre/
+bin
+mget *
+bye
+EOF
+
+cd /home/MentorGraphics/Pictures/
+
+ftp -niv $HOST << EOF
+user $UNAME $PSWD
+cd /home/MentorGraphics/mentorsoftware/Pictures/
+bin
+mget *
+bye
+EOF
+
+cd /home/mrcetece/Tanner_EDA/
+ftp -niv $HOST << EOF
+user $UNAME $PSWD
+cd /home/mrcetece/Tanner_EDA/
+bin
+mget *
+bye
+EOF
+chmod -R 777 /home/MentorGraphics/ams
+chmod -R 777 /home/MentorGraphics/tanner
+chmod -R 777 /home/MentorGraphics/nitro
+chmod -R 777 /home/MentorGraphics/calibre
+chmod -R 777 /home/MentorGraphics/cshrc
+chmod -R 777 /home/MentorGraphics/Pictures
+chmod -R 777 /home/mrcetece/Tanner_EDA
