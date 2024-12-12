aabc00f751579907b190da8324b301d75190bfaf
kishana0427 <95524882+kishana0427@users.noreply.github.com>
2024-12-12 11:45:20 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,23 @@
+METHOD-I(Folder with Recursive):
+===============================
+===============================
+#!/bin/bash
+
+# FTP server details
+FTP_SERVER="ftp.example.com"
+FTP_USER="your_username"
+FTP_PASS="your_password"
+REMOTE_DIR="/path/to/remote/folder"
+LOCAL_DIR="/path/to/local/destination"
+
+# Use wget to download the folder recursively
+wget --ftp-user="$FTP_USER" --ftp-password="$FTP_PASS" \
+     -r -nH --cut-dirs=1 --no-parent \
+     -P "$LOCAL_DIR" "ftp://$FTP_SERVER$REMOTE_DIR"
+
+METHOD-II(Only All files):
+===========================
+===========================
 #!/bin/bash
 HOST=mentorserver
 UNAME=mrcetece
